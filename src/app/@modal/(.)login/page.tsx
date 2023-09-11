edb8ecaacf46c8a5f39ab2a8ac3f6e36d3297693
Asep Ardi <ardi.winardi91@gmail.com>
2023-09-11 17:24:00 +0700

    feat(login): add intercept login route

---
+++ src/app/@modal/(.)login/page.tsx
@@ -0,0 +1,11 @@
+import Modal from "@/shared/components/atoms/Modal";
+
+export default function LoginModal() {
+  return (
+    <Modal>
+      <div className="bg-white p-3 rounded-md self-center">
+        TODO: Login Form
+      </div>
+    </Modal>
+  );
+}
